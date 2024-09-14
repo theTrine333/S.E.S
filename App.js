2cc18d5aa6bb509fd48ca1716c056ea3182317dd
theTrine333 <trinekaka@gmail.com>
2024-09-14 13:52:30 +0300

    Dependancies installation: 	modified:   App.js 	modified:   app.json 	modified:   assets/icon.png 	modified:   package-lock.json 	modified:   package.json
     Untracked files:
    	Styles.ts
    	admin/
    	bottomTabs/
    	components/
    	screens/
    	tsconfig.json

--- App.js
+++ App.js
@@ -1,20 +1,24 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { NavigationContainer } from "@react-navigation/native";
+import { createNativeStackNavigator } from "@react-navigation/native-stack";
+import Home from "./screens/home";
+import Central from "./screens/central";
+import Nutrition from "./screens/nutrition";
+import Ingredient from "./screens/ingredient";
+import Diet from "./screens/diet";
+import AdminHome from "./admin/Home";
 
 export default function App() {
+  const Stack = createNativeStackNavigator();
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <NavigationContainer>
+      <Stack.Navigator screenOptions={{ headerShown: false }}>
+        <Stack.Screen name="Diet" component={Diet} />
+        <Stack.Screen name="Home" component={Home} />
+        <Stack.Screen name="AdminHome" component={AdminHome} />
+        <Stack.Screen name="Central" component={Central} />
+        <Stack.Screen name="Nutrition" component={Nutrition} />
+        <Stack.Screen name="Ingredient" component={Ingredient} />
+      </Stack.Navigator>
+    </NavigationContainer>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
